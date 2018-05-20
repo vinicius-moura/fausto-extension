a6ca84d6cbf2c827a477b1e4d9fd480d79c3d559
vinicius-moura <viniciusmdcm@gmail.com>
2018-05-20 01:23:15 -0300

    notifications on click

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 
-window.addEventListener('load', getCurrentTime);
+window.addEventListener('load', showNotification);
 
 //function to get actual hours
 function getCurrentTime() {
   var date = new Date();
-  var hours = date.getHours();
-  console.log(hours);
-  console.log(document.querySelector("[data-hours]"));
-  document.querySelector("[data-hours]").innerHTML = hours;
+  return date.getHours();
+}
+
+function showNotification() {
+  var hours = getCurrentTime();
+  var message = hours + " E POUCO BIXO";
+  var notification = new Notification('SÃO EXATAMENTE', {
+     icon: 'https://static-s.aa-cdn.net/img/gp/20600004678252/Oq2fgWcleIzU0ARnTRkFwNe0dfJDjG_RJSAG70bIn8HhhZkxuybr_UYub93pFx-knUI=w300?v=1',
+     body: message,
+   });
 }
